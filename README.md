187d6465d890d72368bf5b5ccfb8513ec93f0011
Greg Clark <gclark@efcsystems.com>
2018-11-06 08:58:50 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Serverless Bacon Ipsum Generator
+
+Call Bacon Ipsum Generator from our serverless function and return meaty message
+
+API information at: https://baconipsum.com/json-api/
---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+let axios = require('axios');
+
+module.exports.ipsum = async (event, context) => {
+
+  // Calling Bacon Ipsum
+  // https://baconipsum.com/json-api/
+
+  // Type is defined in environment variable
+  const type = process.env.type;
+  let message = '';
+  let statusCode = 200;
+  const queryUrl = `https://baconipsum.com/api/?type=${type}`;
+
+  try {
+    const response = await axios.get(queryUrl);
+    message = response.data[0];
+  } catch (err) {
+    console.log(err);
+    statusCode = 400;
+    message = err;
+  }
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message: message,
+      input: event,
+    }),
+  };
+};
