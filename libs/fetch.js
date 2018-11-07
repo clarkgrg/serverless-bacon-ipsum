5a37e256e55037bd1cc628c67bff57d018e62d35
Greg Clark <gclark@efcsystems.com>
2018-11-07 13:18:01 -0600

    Removed old code

--- libs/fetch.js
+++ libs/fetch.js
@@ -3,8 +3,6 @@ let axios = require('axios');
 const fetchIpsum = async () => {
   // Type is defined in environment variable
   const type = process.env.type;
-  let message = '';
-  let statusCode = 200;
   const queryUrl = `https://baconipsum.com/api/?type=${type}`;
 
   const response = await axios.get(queryUrl);
